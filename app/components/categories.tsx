2c0d6648b9bbc96147395ae4f1ee565d9c77012d
hongjunjeong <hongjunjeong95@gmail.com>
2023-10-27 10:23:27 +0900

    Create categories

--- app/components/categories.tsx
+++ app/components/categories.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div>
+      <div>
+        <Link href="/experiences" className="flex items-center">
+          <img
+            src="https://d2ur7st6jjikze.cloudfront.net/cms_icons/89_original_1657589745.png?1657589745"
+            alt="투어·티켓"
+            className="w-6 mr-2"
+          />
+          <span>투어·티켓</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
